739179ab40e2b06a20718a38283728ea3c1376e0
Dogu Kocatepe <dogukocatepe@std.iyte.edu.tr>
2025-07-12 11:00:46 +0300

    Fix type error in test data generation

--- main.cc
+++ main.cc
@@ -16,7 +16,7 @@ int main(void) {
     float **X, *y;
 
     // Generate ground truth data
-    generate_test_data(X, y, [](int x) { 
+    generate_test_data(X, y, [](float x) { 
         return 2.5382 * cos(x)*x + x*x - 0.5; 
     });
 
