c60ac9b0813ce0c5840118242b20313fb1350896
Dogu Kocatepe <dogukocatepe@std.iyte.edu.tr>
2025-07-21 21:28:08 +0300

    Remove intra-individual namespace from vm_control and vm_propagation

--- vm/vm_control.hpp
+++ vm/vm_control.hpp
@@ -11,109 +11,107 @@
 #include "vm_debug.hpp"
 #include "vm_propagation.hpp"
 
-namespace intra_individual {
-    template <PropagationType propType, ParallelismType paraType> __device__
-    void vm_control(const int tid, const int datapoint_idx,
-                    const Instruction* bytecode, 
-                    const int bytecode_length,
-                    const int m, 
-                    const float *const __restrict__ *const __restrict__ X_d, 
-                    const float *const __restrict__ y_d,
-                    const StackState& s, 
-                    int& program_counter,
-                    float *const __restrict__ weights_d,
-                    float *const __restrict__ *const __restrict__ weights_grad_d)
-    {
-        bool exit = false;
+template <PropagationType propType, ParallelismType paraType> __device__
+void vm_control(const int tid, const int datapoint_idx,
+                const Instruction* bytecode, 
+                const int bytecode_length,
+                const int m, 
+                const float *const __restrict__ *const __restrict__ X_d, 
+                const float *const __restrict__ y_d,
+                const StackState& s, 
+                int& program_counter,
+                float *const __restrict__ weights_d,
+                float *const __restrict__ *const __restrict__ weights_grad_d)
+{
+    bool exit = false;
 
-        for (; !exit && program_counter < bytecode_length; ++program_counter) {
-            Instruction instruction = bytecode[program_counter];
-            switch (instruction.opcode) {
-                /* Operations with immediate operands */
-                case PUSH_IMMEDIATE:
-                    vm_debug_print(tid, "imm %f", instruction.value);
-                    propagate_immediate<propType>(tid, instruction.value, s);
-                    break;
+    for (; !exit && program_counter < bytecode_length; ++program_counter) {
+        Instruction instruction = bytecode[program_counter];
+        switch (instruction.opcode) {
+            /* Operations with immediate operands */
+            case PUSH_IMMEDIATE:
+                vm_debug_print(tid, "imm %f", instruction.value);
+                propagate_immediate<propType>(tid, instruction.value, s);
+                break;
 
-                /* Operations with index operands */
-                case PUSH_VARIABLE:
-                    vm_debug_print(tid, "var %d", instruction.argindex);
-                    if constexpr (paraType == INTRA_INDIVIDUAL) {
-                        propagate_immediate<propType>(tid, X_d[instruction.argindex][tid], s);
-                    }
-                    if constexpr (paraType == INTER_INDIVIDUAL) {
-                        propagate_immediate<propType>(tid, X_d[datapoint_idx][instruction.argindex], s);
-                    }
-                    break;
-                case PUSH_PARAMETER:
-                    vm_debug_print(tid, "param %d", instruction.argindex);
-                    propagate_parameter<propType>(tid, instruction.argindex, s, 
-                        weights_d, weights_grad_d);
-                    break;
+            /* Operations with index operands */
+            case PUSH_VARIABLE:
+                vm_debug_print(tid, "var %d", instruction.argindex);
+                if constexpr (paraType == INTRA_INDIVIDUAL) {
+                    propagate_immediate<propType>(tid, X_d[instruction.argindex][tid], s);
+                }
+                if constexpr (paraType == INTER_INDIVIDUAL) {
+                    propagate_immediate<propType>(tid, X_d[datapoint_idx][instruction.argindex], s);
+                }
+                break;
+            case PUSH_PARAMETER:
+                vm_debug_print(tid, "param %d", instruction.argindex);
+                propagate_parameter<propType>(tid, instruction.argindex, s, 
+                    weights_d, weights_grad_d);
+                break;
 
-                /* Binary Operations */
-                case ADD:
-                    vm_debug_print(tid, "add");
-                    propagate<propType, 2>(tid, forward_add, grad_add, s, instruction);
-                    break;
-                case SUB:
-                    vm_debug_print(tid, "sub");
-                    propagate<propType, 2>(tid, forward_sub, grad_sub, s, instruction);
-                    break;
-                case MUL:
-                    vm_debug_print(tid, "mul");
-                    propagate<propType, 2>(tid, forward_mul, grad_mul, s, instruction);
-                    break;
-                case DIV:
-                    vm_debug_print(tid, "div");
-                    propagate<propType, 2>(tid, forward_div, grad_div, s, instruction);
-                    break;
+            /* Binary Operations */
+            case ADD:
+                vm_debug_print(tid, "add");
+                propagate<propType, 2>(tid, forward_add, grad_add, s, instruction);
+                break;
+            case SUB:
+                vm_debug_print(tid, "sub");
+                propagate<propType, 2>(tid, forward_sub, grad_sub, s, instruction);
+                break;
+            case MUL:
+                vm_debug_print(tid, "mul");
+                propagate<propType, 2>(tid, forward_mul, grad_mul, s, instruction);
+                break;
+            case DIV:
+                vm_debug_print(tid, "div");
+                propagate<propType, 2>(tid, forward_div, grad_div, s, instruction);
+                break;
 
-                /* Unary Operations */
-                case SIN:
-                    vm_debug_print(tid, "sin");
-                    propagate<propType, 1>(tid, forward_sin, grad_sin, s, instruction);
-                    break;
-                case COS:
-                    vm_debug_print(tid, "cos");
-                    propagate<propType, 1>(tid, forward_cos, grad_cos, s, instruction);
-                    break;
-                case EXP:
-                    vm_debug_print(tid, "exp");
-                    propagate<propType, 1>(tid, forward_exp, grad_exp, s, instruction);
-                    break;
+            /* Unary Operations */
+            case SIN:
+                vm_debug_print(tid, "sin");
+                propagate<propType, 1>(tid, forward_sin, grad_sin, s, instruction);
+                break;
+            case COS:
+                vm_debug_print(tid, "cos");
+                propagate<propType, 1>(tid, forward_cos, grad_cos, s, instruction);
+                break;
+            case EXP:
+                vm_debug_print(tid, "exp");
+                propagate<propType, 1>(tid, forward_exp, grad_exp, s, instruction);
+                break;
 
-                /* No operation */
-                case NOP:
-                    vm_debug_print(tid, "nop");
-                    break;
+            /* No operation */
+            case NOP:
+                vm_debug_print(tid, "nop");
+                break;
 
-                /* Loss function evaluation */
-                case LOSS:
-                    vm_debug_print(tid, "loss");
+            /* Loss function evaluation */
+            case LOSS:
+                vm_debug_print(tid, "loss");
 
-                    // Calculate loss and replace the value on the stack with the loss
+                // Calculate loss and replace the value on the stack with the loss
 
-                    float& stack_value = s.stack_d[0][tid];
+                float& stack_value = s.stack_d[0][tid];
 
-                    const float y_predicted = stack_value;
-                    const float y_target = y_d[tid];
+                const float y_predicted = stack_value;
+                const float y_target = y_d[tid];
 
-                    stack_value = (y_predicted - y_target) / m;
+                stack_value = (y_predicted - y_target) / m;
 
-                    // Forward propagation has finished.
+                // Forward propagation has finished.
 
-                    exit = true;
-                    break;
-            }
-
-            vm_debug_print_stack(tid, s);
+                exit = true;
+                break;
         }
 
-        if constexpr (propType == FORWARD) {
-            if (!exit) {
-                assert(false && "Compiled program finished without encountering loss function.");
-            }
+        vm_debug_print_stack(tid, s);
+    }
+
+    if constexpr (propType == FORWARD) {
+        if (!exit) {
+            assert(false && "Compiled program finished without encountering loss function.");
         }
     }
 }
--- vm/vm_propagation.hpp
+++ vm/vm_propagation.hpp
@@ -8,110 +8,108 @@
 
 #include "../compiler/ir.hpp"
 
-namespace intra_individual {
-    __device__
-    static inline void push_stack(const StackState &s, int tid, float value) {
-        s.stack_d[s.stack_pointer++][tid] = value;
+__device__
+static inline void push_stack(const StackState &s, int tid, float value) {
+    s.stack_d[s.stack_pointer++][tid] = value;
+}
+
+__device__
+static inline float pop_stack(const StackState &s, int tid) {
+    return s.stack_d[--s.stack_pointer][tid];
+}
+
+__device__
+static inline void push_intermediate(const StackState &s, int tid, float value) {
+    s.intermediate_d[s.intermediate_pointer++][tid] = value;
+}
+
+__device__
+static inline float read_intermediate(const StackState &s, int tid, int index) {
+    return s.intermediate_d[index][tid];
+}
+
+template <PropagationType proptype> __device__
+static inline void propagate_immediate(int tid, const float& immediate, const StackState& s) {
+    if constexpr (proptype == FORWARD) {
+        push_stack(s, tid, immediate);
     }
 
-    __device__
-    static inline float pop_stack(const StackState &s, int tid) {
-        return s.stack_d[--s.stack_pointer][tid];
+    if constexpr (proptype == BACK) {
+        // Pop gradient from stack, discard the value.
+        pop_stack(s, tid);
+    }
+}
+
+template <PropagationType proptype> __device__
+static inline void propagate_parameter(int tid, const int& param_index, const StackState& s,
+                                    float *const __restrict__ weights, 
+                                    float *const __restrict__ *const __restrict__ weights_grad_d) {
+    if constexpr (proptype == FORWARD) {
+        push_stack(s, tid, weights[param_index]);
     }
 
-    __device__
-    static inline void push_intermediate(const StackState &s, int tid, float value) {
-        s.intermediate_d[s.intermediate_pointer++][tid] = value;
-    }
+    if constexpr (proptype == BACK) {
+        // Pop gradient from stack
+        const float incoming_grad = pop_stack(s, tid);
+        vm_debug_print(tid, "  incoming_grad=%f", incoming_grad);
 
-    __device__
-    static inline float read_intermediate(const StackState &s, int tid, int index) {
-        return s.intermediate_d[index][tid];
+        // Add gradient to the total gradient of the associated trainable parameter
+        weights_grad_d[param_index][tid] += incoming_grad;
     }
+}
 
-    template <PropagationType proptype> __device__
-    static inline void propagate_immediate(int tid, const float& immediate, const StackState& s) {
-        if constexpr (proptype == FORWARD) {
-            push_stack(s, tid, immediate);
+template <PropagationType proptype, int opcount, typename F, typename G> __device__ 
+static inline void propagate(int tid, F operation, G gradient, const StackState& s, const Instruction& inst) {
+    if constexpr (proptype == FORWARD) {
+        // Pop operands from the stack
+        float operands[opcount];
+        #pragma unroll
+        for (int k = opcount - 1; k >= 0; k--) {
+            operands[k] = pop_stack(s, tid);
         }
 
-        if constexpr (proptype == BACK) {
-            // Pop gradient from stack, discard the value.
-            pop_stack(s, tid);
+        // Apply operation and push the result to the stack
+        push_stack(s, tid, operation(operands));
+
+        // Save consumed operand as intermediate value for backpropagation
+        #pragma unroll
+        for (int k = 0; k < opcount; ++k) {
+            push_intermediate(s, tid, operands[k]);
         }
     }
 
-    template <PropagationType proptype> __device__
-    static inline void propagate_parameter(int tid, const int& param_index, const StackState& s,
-                                        float *const __restrict__ weights, 
-                                        float *const __restrict__ *const __restrict__ weights_grad_d) {
-        if constexpr (proptype == FORWARD) {
-            push_stack(s, tid, weights[param_index]);
+    if constexpr (proptype == BACK) {
+        // Pop incoming gradient from the stack
+        const float incoming_grad = pop_stack(s, tid);
+        vm_debug_print(tid, "  incoming_grad=%f", incoming_grad);
+
+        // Pop intermediate calculation results from forward propagation from intermediate_d
+        float intermediates[opcount];
+        #pragma unroll
+        for (int k = opcount - 1; k >= 0; k--) {
+            const float intermediate = read_intermediate(s, tid, inst.intermediate_index + k);
+            intermediates[k] = intermediate;
+            vm_debug_print(tid, "  intermediates[%d]=%f", k, intermediate);
         }
 
-        if constexpr (proptype == BACK) {
-            // Pop gradient from stack
-            const float incoming_grad = pop_stack(s, tid);
-            vm_debug_print(tid, "  incoming_grad=%f", incoming_grad);
-
-            // Add gradient to the total gradient of the associated trainable parameter
-            weights_grad_d[param_index][tid] += incoming_grad;
-        }
-    }
-
-    template <PropagationType proptype, int opcount, typename F, typename G> __device__ 
-    static inline void propagate(int tid, F operation, G gradient, const StackState& s, const Instruction& inst) {
-        if constexpr (proptype == FORWARD) {
-            // Pop operands from the stack
-            float operands[opcount];
-            #pragma unroll
-            for (int k = opcount - 1; k >= 0; k--) {
-                operands[k] = pop_stack(s, tid);
-            }
-
-            // Apply operation and push the result to the stack
-            push_stack(s, tid, operation(operands));
-
-            // Save consumed operand as intermediate value for backpropagation
-            #pragma unroll
-            for (int k = 0; k < opcount; ++k) {
-                push_intermediate(s, tid, operands[k]);
-            }
+        // Calculate local gradients
+        float local_grad[opcount];
+        gradient(intermediates, local_grad);
+        #pragma unroll
+        for (int k = opcount - 1; k >= 0; k--) {
+            vm_debug_print(tid, "  local_grad[%d]=%f", k, local_grad[k]);
         }
 
-        if constexpr (proptype == BACK) {
-            // Pop incoming gradient from the stack
-            const float incoming_grad = pop_stack(s, tid);
-            vm_debug_print(tid, "  incoming_grad=%f", incoming_grad);
+        // Calculate outgoing gradients
+        #pragma unroll
+        for (int k = opcount - 1; k >= 0; k--) {
+            local_grad[k] *= incoming_grad;
+        }
 
-            // Pop intermediate calculation results from forward propagation from intermediate_d
-            float intermediates[opcount];
-            #pragma unroll
-            for (int k = opcount - 1; k >= 0; k--) {
-                const float intermediate = read_intermediate(s, tid, inst.intermediate_index + k);
-                intermediates[k] = intermediate;
-                vm_debug_print(tid, "  intermediates[%d]=%f", k, intermediate);
-            }
-
-            // Calculate local gradients
-            float local_grad[opcount];
-            gradient(intermediates, local_grad);
-            #pragma unroll
-            for (int k = opcount - 1; k >= 0; k--) {
-                vm_debug_print(tid, "  local_grad[%d]=%f", k, local_grad[k]);
-            }
-
-            // Calculate outgoing gradients
-            #pragma unroll
-            for (int k = opcount - 1; k >= 0; k--) {
-                local_grad[k] *= incoming_grad;
-            }
-
-            // Push outgoing gradients to the stack
-            #pragma unroll
-            for (int k = opcount - 1; k >= 0; k--) {
-                push_stack(s, tid, local_grad[k]);
-            }
+        // Push outgoing gradients to the stack
+        #pragma unroll
+        for (int k = opcount - 1; k >= 0; k--) {
+            push_stack(s, tid, local_grad[k]);
         }
     }
 }
