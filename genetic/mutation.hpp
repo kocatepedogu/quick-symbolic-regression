3107d5037ff59d63d55e79e8d7a0d1aed1184612
Dogu Kocatepe <dogukocatepe@std.iyte.edu.tr>
2025-07-25 21:11:11 +0300

    Reimplement mutation as crossover with a randomly generated tree

--- genetic/mutation.hpp
+++ genetic/mutation.hpp
@@ -2,22 +2,21 @@
 #define MUTATION_HPP
 
 #include "../expressions/expression.hpp"
+#include "crossover.hpp"
 #include "expression_generator.hpp"
 
 class Mutation {
 public:
     constexpr Mutation(int nvars, int nweights, int max_depth, float mutation_probability)  :
         expression_generator(nvars, nweights, max_depth), 
-        mutation_probability(mutation_probability) {}
+        crossover(mutation_probability) {}
 
     Expression mutate(const Expression &expr) noexcept;
 
 private:
-    Expression mutate(const Expression &node, int& current, int target) noexcept;
-
     ExpressionGenerator expression_generator;
 
-    const float mutation_probability;
+    Crossover crossover;
 };
 
 #endif
