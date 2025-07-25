1168ef727126351d0d07085f94c32ebf77b76800
Dogu Kocatepe <dogukocatepe@std.iyte.edu.tr>
2025-07-25 10:46:20 +0300

    Add python bindings and python benchmark script

---
+++ benchmark.py
@@ -0,0 +1,20 @@
+# SPDX-FileCopyrightText: 2025 Doğu Kocatepe
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import libquicksr
+import numpy as np
+
+X = []
+for x0 in np.linspace(-5, 5, 64):
+    for x1 in np.linspace(-5, 5, 64):
+        X.append([x0, x1])
+
+y = [x0*np.exp(x0 + x1) + np.sin(x1) 
+     for x0, x1 in X]
+
+libquicksr.fit(
+    X, y, 
+    nthreads=1, 
+    nweights=1, 
+    npopulation=20000, 
+    ngenerations=2)
---
+++ python/bindings.cc
@@ -0,0 +1,91 @@
+// SPDX-FileCopyrightText: 2025 Doğu Kocatepe
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#include <pybind11/pybind11.h>
+#include <pybind11/numpy.h>
+
+#include "../genetic/island.hpp"
+
+namespace py = pybind11;
+
+void fit(py::array_t<float> numpy_X, py::array_t<float> numpy_y, 
+        int nweights,
+        int npopulation, int ngenerations,
+        int max_initial_depth, int max_mutation_depth,
+        float mutation_probability,
+        int nthreads) 
+{
+    auto numpy_X_buffer_info = numpy_X.request();
+    auto numpy_y_buffer_info = numpy_y.request();
+
+    if (numpy_X_buffer_info.ndim != 1 && numpy_X_buffer_info.ndim != 2) {
+        throw py::value_error("Argument X (feature matrix) must be an either one- or two-dimensional array.");
+    }
+
+    if (numpy_y_buffer_info.ndim != 1) {
+        throw py::value_error("Argument y (target vector) must be a one-dimensional array.");
+    }
+
+    if (numpy_X_buffer_info.shape[0] != numpy_y_buffer_info.shape[0]) {
+        throw py::value_error("First dimensions of X and y must match (the number of data points)");
+    }
+
+    // Number of rows / data points
+    ssize_t num_data_points = numpy_X_buffer_info.shape[0];
+
+    // Number of columns / features
+    ssize_t num_features = numpy_X_buffer_info.ndim == 1 ? 1 : numpy_X_buffer_info.shape[1];
+
+    // Allocate and fill X
+    float **X = (float **)malloc(num_data_points * sizeof *X);
+    for (int i = 0; i < num_data_points; ++i) {
+        X[i] = (float *)malloc(num_features * sizeof **X);
+        for (int j = 0; j < num_features; ++j) {
+            X[i][j] = (static_cast<float*>(numpy_X_buffer_info.ptr))[i * num_features + j];
+        }
+    }
+
+    // Allocate and fill y
+    float *y = (float *)malloc(num_data_points * sizeof *y);
+    for (int i = 0; i < num_data_points; ++i) {
+        y[i] = (static_cast<float*>(numpy_y_buffer_info.ptr))[i];
+    }
+
+    // Create dataset
+    Dataset dataset(X, y, num_data_points, num_features);
+
+    // Fit
+    #pragma omp parallel num_threads(nthreads)
+    {
+        // Create island
+        Island island(dataset, nweights, npopulation / nthreads, 
+            max_initial_depth, max_mutation_depth, mutation_probability);
+
+        // Iterate island
+        island.iterate(ngenerations);
+    }
+
+    // Free y
+    free(y);
+
+    // Free X
+    for (int i = 0; i < num_data_points; ++i) {
+        free(X[i]);
+    }
+    free(X);
+}
+
+PYBIND11_MODULE(libquicksr, m) {
+    m.doc() = "pybind11 libquicksr plugin";
+    m.def("fit", &fit, "Fits a symbollic expression to given feature matrix X and target vector y",
+        py::arg("X"),                               // Required argument
+        py::arg("y"),                               // Required argument
+        py::arg("nweights") = 2,                    // Optional argument with default value
+        py::arg("npopulation") = 1000,              // Optional argument with default value
+        py::arg("ngenerations") = 10,               // Optional argument with default value
+        py::arg("max_initial_depth") = 3,           // Optional argument with default value
+        py::arg("max_mutation_depth") = 3,          // Optional argument with default value
+        py::arg("max_mutation_probability") = 0.7,  // Optional argument with default value
+        py::arg("nthreads") = 1                     // Optional argument with default value
+    );
+}
