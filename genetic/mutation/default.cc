ac0365ed92d9375cd09d466d70f84e993468fbf6
Dogu Kocatepe <dogukocatepe@std.iyte.edu.tr>
2025-07-28 21:34:02 +0300

    Improve mutation to increase diversity

--- genetic/mutation/default.cc
+++ genetic/mutation/default.cc
@@ -4,6 +4,13 @@ Expression DefaultMutation::mutate(const Expression &expr) noexcept {
     const auto &random_expr = expression_generator.generate();
     const auto &result_pair = crossover.crossover(expr, random_expr);
 
-    return get<0>(result_pair);
+    // Substitute random subtree into the original expression
+    if (rand() % 2 == 0) {
+        return get<0>(result_pair);
+    }
+    // Substitute original expression into the random subtree
+    else {
+        return get<1>(result_pair);
+    }
 }
 
