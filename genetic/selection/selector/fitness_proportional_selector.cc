fd6bdd8311515bcb435b33b7081a78991ff9fe27
Dogu Kocatepe <dogukocatepe@std.iyte.edu.tr>
2025-07-28 20:42:01 +0300

    Fix fitness proportional selection

--- genetic/selection/selector/fitness_proportional_selector.cc
+++ genetic/selection/selector/fitness_proportional_selector.cc
@@ -1,40 +1,33 @@
 #include "fitness_proportional_selector.hpp"
 
-#include "../../../util/rng.hpp"
+#include <random>
 
 void FitnessProportionalSelector::update(const Expression population[]) {
-    float min_fitness = 1e30;
-    for (int i = 0; i < npopulation; ++i) {
-        float fitness = -population[i].loss;
-        if (fitness < min_fitness) {
-            min_fitness = fitness;
-        }
+    // Step 1: Calculate the total fitness
+    float totalFitness = 0.0f;
+    for (size_t i = 0; i < npopulation; ++i) {
+        // Convert loss to fitness (reciprocal)
+        float fitness = 1.0f / population[i].loss;
+        totalFitness += fitness;
     }
 
-    float sum_of_fitnesses = 0;
-    for (int i = 0; i < npopulation; ++i) {
-        float fitness = (-population[i].loss) - min_fitness;
-        sum_of_fitnesses += fitness;
-    }
-
-    // Compute selection probabilities for each individual
-    for (int i = 0; i < npopulation; ++i) {
-        probabilities[i] = ((-population[i].loss) - min_fitness) / sum_of_fitnesses;
+    // Step 2: Normalize fitness values to get probabilities
+    for (size_t i = 0; i < npopulation; ++i) {
+        float fitness = 1.0f / population[i].loss;
+        probabilities[i] = fitness / totalFitness;
     }
 }
 
 
 const Expression& FitnessProportionalSelector::select(const Expression population[]) {
-    float u = (thread_local_rng() % RAND_MAX) / (float)RAND_MAX;
-    float s = 0;
+    // Create a random number generator
+    static thread_local std::random_device rd;
+    static thread_local std::mt19937 gen(rd());
+    
+    std::discrete_distribution<> distribution(
+        probabilities.begin(), probabilities.end());
 
-    int parent_index = 0;
-    for (; parent_index < npopulation && u > s; ++parent_index) {
-        s += probabilities[parent_index];
-    }
-
-    if (parent_index < 0) parent_index = 0;
-    if (parent_index > npopulation - 1) parent_index = npopulation - 1;
-
-    return population[parent_index];
+    // Select an expression based on the probabilities
+    size_t selectedIndex = distribution(gen);
+    return population[selectedIndex];
 }
