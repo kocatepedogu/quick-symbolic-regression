922d7c14570737be4dd5bb7a042258446158803e
Dogu Kocatepe <dogukocatepe@std.iyte.edu.tr>
2025-07-30 19:27:55 +0300

    Modify mutation to preserve values of trainable weights

--- genetic/mutation/default.cc
+++ genetic/mutation/default.cc
@@ -4,13 +4,21 @@ Expression DefaultMutation::mutate(const Expression &expr) noexcept {
     const auto &random_expr = expression_generator.generate();
     const auto &result_pair = crossover.crossover(expr, random_expr);
 
-    // Substitute random subtree into the original expression
+    Expression result = 0.0;
+
+    // Substitute random subtree into the original expression (50% chance)
     if (rand() % 2 == 0) {
-        return get<0>(result_pair);
+        result = get<0>(result_pair);
     }
-    // Substitute original expression into the random subtree
+    // Substitute original expression into the random subtree (50% chance)
     else {
-        return get<1>(result_pair);
+        result = get<1>(result_pair);
     }
+
+    // Use the same weights as the original expression
+    result.weights = expr.weights;
+
+    // Return the mutated expression
+    return result;
 }
 
