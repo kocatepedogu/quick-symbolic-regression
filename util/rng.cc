e0312ff28eb5997f52fe017bb19fb91fd190ebca
Dogu Kocatepe <dogukocatepe@std.iyte.edu.tr>
2025-08-16 08:17:49 +0300

    Seed thread local RNG with time

--- util/rng.cc
+++ util/rng.cc
@@ -2,10 +2,11 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 #include <random>
+#include <chrono>
 
 namespace qsr {
 
-constexpr int random_seed = 42;
+static thread_local unsigned random_seed = static_cast<unsigned>(std::chrono::system_clock::now().time_since_epoch().count());
 
 thread_local std::mt19937 thread_local_rng(random_seed);
 
