e6895ebe7e210364a4b3f25b97932727e7d4393e
Dogu Kocatepe <dogukocatepe@std.iyte.edu.tr>
2025-08-16 07:43:01 +0300

    Fix license identifier in `quicksr.py`

--- quicksr.py
+++ quicksr.py
@@ -1,6 +1,5 @@
-
-// SPDX-FileCopyrightText: 2025 Doğu Kocatepe
-// SPDX-License-Identifier: GPL-3.0-or-later
+# SPDX-FileCopyrightText: 2025 Doğu Kocatepe
+# SPDX-License-Identifier: GPL-3.0-or-later
 
 import libquicksr
 import numpy as np
