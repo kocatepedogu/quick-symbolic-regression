e5c9221814238b16526bd117c64d2f30e6cce344
Dogu Kocatepe <dogukocatepe@std.iyte.edu.tr>
2025-08-23 22:13:38 +0300

    Refactor intra-individual Program class and add iterator for accessing individual programs

--- runners/intra-individual/program/program.cc
+++ runners/intra-individual/program/program.cc
@@ -9,47 +9,78 @@
 #include <hip/hip_runtime.h>
 
 namespace qsr::intra_individual {
-   Program::Program(const std::vector<Expression>& exp_pop) {
-        const int num_of_individuals = exp_pop.size();
+   Program::Program(const std::vector<Expression>& population) {
+        const int num_of_individuals = population.size();
+
+        // Allocate arrays for storing number of instructions, stack requirements, 
+        // and intermediate requirements for every program
+        allocate_arrays(num_of_individuals);
+
+        // Compile all expressions to intermediate representation
+        compile_expressions(population);
+
+        // Find the longest IR to determine the maximum number of instructions
+        find_longest_ir();
+
+        // Copy all programs to GPU memory
+        copy_to_gpu_memory();
+    }
+
+    void Program::allocate_arrays(const int num_of_individuals) {
         this->num_of_individuals = num_of_individuals;
 
         // Create array for storing number of instructions in each program
         this->num_of_instructions = Array1D<int>(num_of_individuals);
 
-        // Create array for storing stack requirements for each program
+        // Create array for storing stack requirements of each program
         this->stack_req = Array1D<int>(num_of_individuals);
 
-        // Create array for storing intermediate requirements for each program
+        // Create array for storing intermediate requirements of each program
         this->intermediate_req = Array1D<int>(num_of_individuals);
+    }
 
-        // Compile all expressions to IR
-        std::vector<IntermediateRepresentation> irs(num_of_individuals);
+    void Program::compile_expressions(const std::vector<Expression>& population) {
+        // Resize the IRs vector to accommodate all individuals
+        irs.resize(num_of_individuals);
+
+        // Loop through each expression in the population
         for (int i = 0; i < num_of_individuals; ++i) {
-            const auto &ir = irs[i] = compile(exp_pop[i]);
+            // Compile each expression to intermediate representation
+            const auto &ir = irs[i] = compile(population[i]);
+
+            // Store the number of instructions, stack requirement, and intermediate requirement for each program
             num_of_instructions.ptr[i] = ir.bytecode.size();
             stack_req.ptr[i] = ir.stack_requirement;
             intermediate_req.ptr[i] = ir.intermediate_requirement;
         }
+    }
 
-        // Find the longest IR to determine the maximum number of instructions
-        int max_num_of_instructions = 0;
+    void Program::find_longest_ir() {
+        // Find the maximum number of instructions
+        max_num_of_instructions = 0;
         for (const auto& ir : irs) {
             if (ir.bytecode.size() > max_num_of_instructions) {
                 max_num_of_instructions = ir.bytecode.size();
             }
         }
+    }
 
+    void Program::copy_to_gpu_memory() {
         // Create array for storing pointers to individual programs
         this->bytecode = Array2D<Instruction>(num_of_individuals, max_num_of_instructions);
 
-        // Compile every expression to IR and copy to GPU memory
-        for (int i = 0; i < num_of_individuals; ++i) {
-            // Compile
-            const IntermediateRepresentation& ir = irs[i];
-            const int num_of_instructions = ir.bytecode.size();
-            
-            // Copy program contents to GPU
-            memcpy(this->bytecode.ptr[i], ir.bytecode.data(), num_of_instructions * sizeof *ir.bytecode.data());
+        // Copy instructions from all expressions to GPU memory
+        for (int i = 0; i < num_of_individuals; ++i) {     
+            auto& ir = irs[i];
+            memcpy(this->bytecode.ptr[i], ir.bytecode.data(), ir.bytecode.size() * sizeof *ir.bytecode.data());
         }
+    }
+
+    ProgramIterator Program::begin() {
+        return ProgramIterator(this);
+    }
+
+    ProgramIterator Program::end() {
+        return ProgramIterator(this, num_of_individuals);
     }
 }
--- runners/intra-individual/program/program.hpp
+++ runners/intra-individual/program/program.hpp
@@ -13,7 +13,22 @@
 #include <vector>
 
 namespace qsr::intra_individual {
-    struct Program {
+    struct ProgramIterator;
+
+    /**
+     * @brief Stores bytecode instructions of a population of programs for intra-individual GPU execution.
+     *
+     * @details
+     * Although the intra-individual mode executes each expression one-by-one in a sequential manner,
+     * it is still beneficial (up to 4x speedup) to compile all expressions at once and then store all 
+     * programs in a single contiguous block of memory due to cache behavior.
+     * 
+     * This class stores instructions in a 2D array (continuous in memory) with dimensions
+     * [num_of_individuals, max_num_of_instructions]. The other information is stored in 1D arrays.
+     *
+     */
+    class Program {
+    public:
         /// Programs
         Array2D<Instruction> bytecode;
 
@@ -29,11 +44,96 @@ namespace qsr::intra_individual {
         /// Total number of individuals
         int num_of_individuals;
 
+        /// Length of the longest program
+        int max_num_of_instructions;
+
         /// Constructor
         Program(const std::vector<Expression>& exp_pop);
 
         /// Default constructor
         Program() = default;
+
+        /// Begin iterator for accessing individual programs
+        ProgramIterator begin();
+
+        /// End iterator for accessing individual programs
+        ProgramIterator end();
+
+    private:
+        void allocate_arrays(const int num_of_individuals);
+
+        void compile_expressions(const std::vector<Expression>& exp_pop);
+
+        void find_longest_ir();
+
+        void copy_to_gpu_memory();
+
+        std::vector<IntermediateRepresentation> irs;
+    };
+
+    /**
+     * @brief Represents an individual program
+     *
+     * @details
+     * While the `Program` class stores information about programs in a structure-of-arrays 
+     * form, this struct provides an array-of-structures view of the same
+     * data for convenience.
+     *
+     * @note
+     * This struct is intended to be used with iterators
+     */
+    struct ProgramIndividual {
+        Ptr1D<Instruction> bytecode;
+
+        int num_of_instructions;
+
+        int stack_req;
+
+        int intermediate_req;
+
+        int index;
+    };
+
+    /**
+     * @brief Iterator for accessing individual programs in a `Program` object.
+     *
+     * @details
+     * This iterator allows for easy access to individual programs in a `Program` object.
+     * It provides an array-of-structures view of the same data for convenience.
+     *
+     * @note
+     * This iterator is intended to be used with the `Program` class and should not be used directly
+     */
+    struct ProgramIterator {
+        inline ProgramIterator(Program *program) : 
+            program(program), index(0) {}
+
+        inline ProgramIterator(Program *program, int index) : 
+            program(program), index(index) {}
+
+        inline const ProgramIndividual operator*() {
+            return ProgramIndividual{
+                .bytecode = program->bytecode.ptr(index),
+                .num_of_instructions = program->num_of_instructions.ptr[index],
+                .stack_req = program->stack_req.ptr[index],
+                .intermediate_req = program->intermediate_req.ptr[index],
+                .index = index
+            };
+        }
+
+        inline ProgramIterator &operator++() {
+            ++index;
+            return *this;
+        }
+
+        inline bool operator!=(ProgramIterator &rhs) {
+            return index != rhs.index;
+        }
+
+    private:
+        Program *program;
+
+        int index;
     };
 }
 
