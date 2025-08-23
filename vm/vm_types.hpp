d28becaa0073b49443a1c8c3ded89d00a1c0798d
Dogu Kocatepe <dogukocatepe@std.iyte.edu.tr>
2025-08-23 23:22:33 +0300

    Remove unused typedefs

--- vm/vm_types.hpp
+++ vm/vm_types.hpp
@@ -4,28 +4,13 @@
 #ifndef INTRA_VM_TYPES_HPP
 #define INTRA_VM_TYPES_HPP
 
-#include "../compiler/ir.hpp"
-
 #include "../../../util/arrays/array1d.hpp"
 #include "../../../util/arrays/array2d.hpp"
 
 namespace qsr {
 
-typedef Instruction *inst_1d_mut;
-typedef Instruction *const __restrict__ inst_1d;
-typedef const Instruction *const __restrict__ c_inst_1d;
-
-typedef Instruction **inst_2d_mut;
-typedef Instruction *const __restrict__ *const __restrict inst_2d;
-typedef const Instruction *const __restrict__ *const __restrict c_inst_2d;
-
-typedef float *real_1d_mut;
 typedef float *const __restrict__ real_1d;
 typedef const float *const __restrict__ c_real_1d;
-
-typedef float **real_2d_mut;
-typedef float *const __restrict__ *const __restrict__ real_2d;
-typedef const float *const __restrict__ *const __restrict__ c_real_2d;
 
 enum ParallelismType {
     INTRA_INDIVIDUAL,
